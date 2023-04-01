1af1e945d48b3ee7d339bf556f1233b483ac1d01
sebassg7 <sebastlopez@unicauca.edu.co>
2023-04-01 18:37:34 -0500

    first commit

--- proyecto-js-matematicas.js
+++ proyecto-js-matematicas.js
@@ -8,116 +8,94 @@ const resultadoVacaciones = document.querySelector('#holidays');
 const resultadoTotalPrestaciones = document.querySelector('#total');
 
 
-botonCalcular.addEventListener('click', calculoCesantias);
-botonCalcular.addEventListener('click', interesesCesantias);
-botonCalcular.addEventListener('click', liquidacionPrimaPrimerSemestre);
-botonCalcular.addEventListener('click', liquidacionVacaiones);
+botonCalcular.addEventListener('click', calculoPrestaciones);
+
 
 
 const auxTransporte = 140606;
 const salarioMinimo = 1300606;  
 
 
-let resultados = [];
-
-
-
-// let totalResultadosCalculados = resultados.reduce((a, b) => a + b, 0);
-// console.log(totalResultadosCalculados);
-
-function calculoCesantias () {
+function calculoPrestaciones () {
     
+    let resultados = [];
     
+
+    
+
+    // CÁLCULO DE CESANTIAS
+
     const salario = Number(inputSalario.value);
     const diasLaborados = Number(inputDiasLaborados.value);  
 
     const salarioPlusAuxTransporte = salario + auxTransporte;
-    console.log(salarioPlusAuxTransporte);
     const limiteSalario = salarioMinimo * 2;
 
     if(salarioPlusAuxTransporte > limiteSalario ){
         console.warn('No es beneficiario de Auxilio de Transporte');
         const diasYearLaborales = 360;
         const cesantiasSinAuxilio = ((salario) * diasLaborados)/diasYearLaborales;
-        resultadoCesantias.innerText = '$ ' + cesantiasSinAuxilio + ' COP' + ' No es beneficiario de Auxilio de Transporte';
         resultados.push(cesantiasSinAuxilio);
-        // return cesantiasSinAuxilio;
+        resultadoCesantias.innerText = '$ ' + cesantiasSinAuxilio + ' COP' + ' No es beneficiario de Auxilio de Transporte';
 
     } else if (salarioPlusAuxTransporte <= limiteSalario){
         const diasYearLaborales = 360;
         const cesantiasConAuxilio = ((salario + auxTransporte) * diasLaborados)/diasYearLaborales;
-        resultadoCesantias.innerText = '$ ' + cesantiasConAuxilio + ' COP';
         resultados.push(cesantiasConAuxilio);
-        // return cesantiasConAuxilio;
+        resultadoCesantias.innerText = '$ ' + cesantiasConAuxilio + ' COP';
 
     } else {
         resultadoCesantias.innerText = 'Hay un error en los datos'
-        // console.warn('Hay un error en los datos');
+
     };
-};
+         
+    
+    // CÁLCULO INTERESES A LAS CESANTIAS 
 
-function interesesCesantias (){
+        const diasYearLaboralesInteresesCesantias = 360;
+        const cesantias = ((salario + auxTransporte) * diasLaborados)/diasYearLaboralesInteresesCesantias;
+        const interesAnualCesantias = (12/100);
+        const interesesSobreCesantias = (cesantias * diasLaborados*interesAnualCesantias)/360;
+        resultados.push(interesesSobreCesantias);
+        resultadoInteresesCesantias.innerText = '$ ' + interesesSobreCesantias + ' COP';
     
-    const salario = Number(inputSalario.value);
-    const diasLaborados = Number(inputDiasLaborados.value);  
-    
-    const diasYearLaborales = 360;
-    const cesantias = ((salario + auxTransporte) * diasLaborados)/diasYearLaborales;
-    const interesAnualCesantias = (12/100);
-    const interesesSobreCesantias = (cesantias * diasLaborados*interesAnualCesantias)/360;
-    resultadoInteresesCesantias.innerText = '$ ' + interesesSobreCesantias + ' COP';
-    resultados.push(interesesSobreCesantias);
-    // return interesesSobreCesantias;
-};
+    // CÁLCULO PRIMA
 
-function liquidacionPrimaPrimerSemestre (){
-    
-    const salario = Number(inputSalario.value);
-    const diasLaborados = Number(inputDiasLaborados.value);  
-    
-    const diasYearLaborales = 360;
+    const diasYearLaboralesPrima = 360;
+   
     if (diasLaborados <= 180){
-        const primaPrimerSemestre = ((salario + auxTransporte) * diasLaborados)/diasYearLaborales;
-        resultadoPrima.innerText = '$ ' + primaPrimerSemestre + ' COP';
+        const primaPrimerSemestre = ((salario + auxTransporte) * diasLaborados)/diasYearLaboralesPrima;
         resultados.push(primaPrimerSemestre);
-        // console.log(primaPrimerSemestre);
-        // return  primaPrimerSemestre;
+        resultadoPrima.innerText = '$ ' + primaPrimerSemestre + ' COP';
+        
     } else if (diasLaborados > 180){
-        const primaPrimerSemestre = ((salario + auxTransporte) * diasLaborados)/diasYearLaborales;
-        // console.log(primaPrimerSemestre);
-        // const primaSegundoSemestre = ((salario + auxTransporte) * diasLaborados)/diasYearLaborales;
-        // console.log(primaSegundoSemestre);
+        const primaPrimerSemestre = ((salario + auxTransporte) * diasLaborados)/diasYearLaboralesPrima;
         const totalPrima = primaPrimerSemestre;
-        resultadoPrima.innerText = '$ ' + totalPrima + ' COP';
         resultados.push(totalPrima);
-        // return  totalPrima;
-        // console.log(totalPrima);
+        resultadoPrima.innerText = '$ ' + totalPrima + ' COP';
+    
     } else {
         console.warn('Hay un error en los datos');
     };
+      
+     // CÁLCULO VACACIONES
+
+     const diasYearLaboralesVacaciones = 720;
+     const vacaciones = ((salario) * diasLaborados)/diasYearLaboralesVacaciones;
+     resultados.push(vacaciones);
+     resultadoVacaciones.innerText = '$ ' + vacaciones + ' COP';
+    
+
+    // CÁLCULO TOTAL PRESTACIONES
+
+    let suma = 0;
+    resultados.forEach(function(resultado) {
+    suma += resultado;
+    });
+    console.log(resultados);
+
+    console.log("La suma es: " + suma);
+
+    resultadoTotalPrestaciones.innerText = '$ ' + suma + ' COP';
 };
 
-
-function liquidacionVacaiones(){
-    
-    const salario = Number(inputSalario.value);
-    const diasLaborados = Number(inputDiasLaborados.value);  
-    
-    const diasYearLaborales = 720;
-    const vacaciones = ((salario) * diasLaborados)/diasYearLaborales;
-    resultadoVacaciones.innerText = '$ ' + vacaciones + ' COP';
-    resultados.push(vacaciones);
-    return  resultadoVacaciones;
-};
-
-// function totalPrestaciones(){
-//     const totalPrestaciones = resultadoVacaciones;
-//     resultadoTotalPrestaciones.innerText = '$ ' + totalPrestaciones + ' COP';
-    
-// }
-
-// function liquidacionPrimaSegundoSemestre (salario, diasLaborados){
-//     const diasYearLaborales = 360;
-//     const primaSegundoSemestre = ((salario + auxTransporte) * diasLaborados)/diasYearLaborales;
-//     return  primaSegundoSemestre;
-// };
